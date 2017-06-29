d494e08783873b7d7d4236444779c7d154b93cef
perezLamed <perez.lamed.vanniekerk@gmail.com>
2017-06-29 16:04:52 +0200

    Small testing changes

--- src/zPublicClass/Test/pcTest_ConfigData.cs
+++ src/zPublicClass/Test/pcTest_ConfigData.cs
@@ -9,8 +9,8 @@ namespace LamedalCore.zPublicClass.Test
     [Test_IgnoreCoverage(enCode_TestIgnore.CodeIsUsedForTesting)]
     public sealed class pcTest_ConfigData
     {
-        public string Folder_TestCase_About = "Please specify the folder where the Excel test cases are located. Use '/' to separate folders. Path should end with '/'";
-        public string Folder_TestCase = "";
+        public string Folder_TestCase_About = "Please specify the folder where the test cases are located. Use '/' to separate folders. Path should end with '/'";
+        public string Folder_TestCase = "c:/projects/lamedalcore/tests/TestData/";
         public string Test_Drive = "C:/";
     }
 }
---
+++ tests/testCases/LamdalCoreXunit_cSharp/ClassNT/Xunit_cSharp.cs
@@ -0,0 +1,13 @@
+﻿using LamedalCore.domain.Attributes;
+using LamedalCore.domain.Enumerals;
+using Xunit;
+
+namespace LamdalCoreXunit_cSharp.ClassNT
+{
+    [Trait("Category", "cSharp")]
+    [BlueprintRule_Class(enBlueprint_ClassNetworkType.XUnitTestMethods)]
+    public partial class Xunit_cSharp
+    {
+        
+    }
+}
